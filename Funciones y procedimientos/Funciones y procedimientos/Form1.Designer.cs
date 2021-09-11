de3958d541a24684673e4a3e2a3026b7c136f79b
fredy-martinez <90357773+fredy-martinez@users.noreply.github.com>
2021-09-11 13:01:19 -0600

    Ejercicio 1
    Funciones y procedimientos

---
+++ Funciones y procedimientos/Funciones y procedimientos/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace Funciones_y_procedimientos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Numero1TextBox = new System.Windows.Forms.TextBox();
+            this.Numero2TextBox = new System.Windows.Forms.TextBox();
+            this.SumaButton = new System.Windows.Forms.Button();
+            this.RestaButton = new System.Windows.Forms.Button();
+            this.MultiplicacionButton = new System.Windows.Forms.Button();
+            this.DividirButton = new System.Windows.Forms.Button();
+            this.LimpiarButton = new System.Windows.Forms.Button();
+            this.ResultadoLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Numero2TextBox);
+            this.groupBox1.Controls.Add(this.Numero1TextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(30, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(458, 166);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Valores";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.LimpiarButton);
+            this.groupBox2.Controls.Add(this.DividirButton);
+            this.groupBox2.Controls.Add(this.MultiplicacionButton);
+            this.groupBox2.Controls.Add(this.RestaButton);
+            this.groupBox2.Controls.Add(this.SumaButton);
+            this.groupBox2.Location = new System.Drawing.Point(509, 27);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(416, 166);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Valores";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.ResultadoLabel);
+            this.groupBox3.Location = new System.Drawing.Point(30, 220);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(895, 166);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Resultado";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numero 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numero 2:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Numero1TextBox
+            // 
+            this.Numero1TextBox.Location = new System.Drawing.Point(124, 53);
+            this.Numero1TextBox.Name = "Numero1TextBox";
+            this.Numero1TextBox.Size = new System.Drawing.Size(223, 26);
+            this.Numero1TextBox.TabIndex = 2;
+            // 
+            // Numero2TextBox
+            // 
+            this.Numero2TextBox.Location = new System.Drawing.Point(124, 107);
+            this.Numero2TextBox.Name = "Numero2TextBox";
+            this.Numero2TextBox.Size = new System.Drawing.Size(223, 26);
+            this.Numero2TextBox.TabIndex = 3;
+            // 
+            // SumaButton
+            // 
+            this.SumaButton.Location = new System.Drawing.Point(26, 36);
+            this.SumaButton.Name = "SumaButton";
+            this.SumaButton.Size = new System.Drawing.Size(104, 37);
+            this.SumaButton.TabIndex = 0;
+            this.SumaButton.Text = "Suma";
+            this.SumaButton.UseVisualStyleBackColor = true;
+            this.SumaButton.Click += new System.EventHandler(this.SumaButton_Click);
+            // 
+            // RestaButton
+            // 
+            this.RestaButton.Location = new System.Drawing.Point(26, 96);
+            this.RestaButton.Name = "RestaButton";
+            this.RestaButton.Size = new System.Drawing.Size(104, 37);
+            this.RestaButton.TabIndex = 1;
+            this.RestaButton.Text = "Resta";
+            this.RestaButton.UseVisualStyleBackColor = true;
+            this.RestaButton.Click += new System.EventHandler(this.RestaButton_Click);
+            // 
+            // MultiplicacionButton
+            // 
+            this.MultiplicacionButton.Location = new System.Drawing.Point(161, 36);
+            this.MultiplicacionButton.Name = "MultiplicacionButton";
+            this.MultiplicacionButton.Size = new System.Drawing.Size(124, 37);
+            this.MultiplicacionButton.TabIndex = 2;
+            this.MultiplicacionButton.Text = "Multiplicacion";
+            this.MultiplicacionButton.UseVisualStyleBackColor = true;
+            this.MultiplicacionButton.Click += new System.EventHandler(this.MultiplicacionButton_Click);
+            // 
+            // DividirButton
+            // 
+            this.DividirButton.Location = new System.Drawing.Point(161, 93);
+            this.DividirButton.Name = "DividirButton";
+            this.DividirButton.Size = new System.Drawing.Size(124, 37);
+            this.DividirButton.TabIndex = 3;
+            this.DividirButton.Text = "Dividir";
+            this.DividirButton.UseVisualStyleBackColor = true;
+            this.DividirButton.Click += new System.EventHandler(this.DividirButton_Click);
+            // 
+            // LimpiarButton
+            // 
+            this.LimpiarButton.Location = new System.Drawing.Point(291, 68);
+            this.LimpiarButton.Name = "LimpiarButton";
+            this.LimpiarButton.Size = new System.Drawing.Size(119, 37);
+            this.LimpiarButton.TabIndex = 4;
+            this.LimpiarButton.Text = "Limpiar";
+            this.LimpiarButton.UseVisualStyleBackColor = true;
+            this.LimpiarButton.Click += new System.EventHandler(this.LimpiarButton_Click);
+            // 
+            // ResultadoLabel
+            // 
+            this.ResultadoLabel.AutoSize = true;
+            this.ResultadoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResultadoLabel.Location = new System.Drawing.Point(415, 77);
+            this.ResultadoLabel.Name = "ResultadoLabel";
+            this.ResultadoLabel.Size = new System.Drawing.Size(108, 25);
+            this.ResultadoLabel.TabIndex = 0;
+            this.ResultadoLabel.Text = "Resultado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 436);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Funciones y Procedimientos";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox Numero2TextBox;
+        private System.Windows.Forms.TextBox Numero1TextBox;
+        private System.Windows.Forms.Button LimpiarButton;
+        private System.Windows.Forms.Button DividirButton;
+        private System.Windows.Forms.Button MultiplicacionButton;
+        private System.Windows.Forms.Button RestaButton;
+        private System.Windows.Forms.Button SumaButton;
+        private System.Windows.Forms.Label ResultadoLabel;
+    }
+}
+
---
+++ Funciones y procedimientos/Funciones y procedimientos/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Funciones_y_procedimientos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LimpiarButton_Click(object sender, EventArgs e)
+        {
+            LimpiarControles();
+        }
+
+        private void LimpiarControles()
+        {
+            Numero1TextBox.Clear();
+            Numero2TextBox.Clear();
+            ResultadoLabel.Text = string.Empty;
+        }
+
+        private int Suma(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        private int Resta(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        private int Multiplicar(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        private int Dividir(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        private void SumaButton_Click(object sender, EventArgs e)
+        {
+            ResultadoLabel.Text = Suma(Convert.ToInt32(Numero1TextBox.Text), Convert.ToInt32(Numero2TextBox.Text)).ToString(); // convertir esa cadena de caracteres a entero.
+        }
+
+        private void RestaButton_Click(object sender, EventArgs e)
+        {
+            ResultadoLabel.Text = Resta(Convert.ToInt32(Numero1TextBox.Text), Convert.ToInt32(Numero2TextBox.Text)).ToString(); // convertir esa cadena de caracteres a entero.
+        }
+
+        private void MultiplicacionButton_Click(object sender, EventArgs e)
+        {
+            ResultadoLabel.Text = Multiplicar(Convert.ToInt32(Numero1TextBox.Text), Convert.ToInt32(Numero2TextBox.Text)).ToString(); // convertir esa cadena de caracteres a entero.
+        }
+
+        private void DividirButton_Click(object sender, EventArgs e)
+        {
+            ResultadoLabel.Text = Dividir(Convert.ToInt32(Numero1TextBox.Text), Convert.ToInt32(Numero2TextBox.Text)).ToString(); // convertir esa cadena de caracteres a entero.
+        }
+    }
+}
