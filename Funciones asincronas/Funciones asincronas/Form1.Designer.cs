e05609fdc796008d0097afbe4879013384ea44d0
fredy-martinez <90357773+fredy-martinez@users.noreply.github.com>
2021-09-20 14:54:42 -0600

    Funcion Asincronica
    Funcion asincronica significa que realiza diferentes procesos al mismo tiempo

---
+++ Funciones asincronas/Funciones asincronas/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace Funciones_asincronas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EjecutarButton = new System.Windows.Forms.Button();
+            this.ResultadoLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SumarButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // EjecutarButton
+            // 
+            this.EjecutarButton.Location = new System.Drawing.Point(40, 77);
+            this.EjecutarButton.Name = "EjecutarButton";
+            this.EjecutarButton.Size = new System.Drawing.Size(134, 60);
+            this.EjecutarButton.TabIndex = 0;
+            this.EjecutarButton.Text = "Ejecutar";
+            this.EjecutarButton.UseVisualStyleBackColor = true;
+            this.EjecutarButton.Click += new System.EventHandler(this.EjecutarButton_Click);
+            // 
+            // ResultadoLabel
+            // 
+            this.ResultadoLabel.AutoSize = true;
+            this.ResultadoLabel.Location = new System.Drawing.Point(62, 177);
+            this.ResultadoLabel.Name = "ResultadoLabel";
+            this.ResultadoLabel.Size = new System.Drawing.Size(82, 20);
+            this.ResultadoLabel.TabIndex = 1;
+            this.ResultadoLabel.Text = "Resultado";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(336, 177);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Resultado";
+            // 
+            // SumarButton
+            // 
+            this.SumarButton.Location = new System.Drawing.Point(314, 77);
+            this.SumarButton.Name = "SumarButton";
+            this.SumarButton.Size = new System.Drawing.Size(134, 60);
+            this.SumarButton.TabIndex = 2;
+            this.SumarButton.Text = "Sumar";
+            this.SumarButton.UseVisualStyleBackColor = true;
+            this.SumarButton.Click += new System.EventHandler(this.SumarButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SumarButton);
+            this.Controls.Add(this.ResultadoLabel);
+            this.Controls.Add(this.EjecutarButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button EjecutarButton;
+        private System.Windows.Forms.Label ResultadoLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button SumarButton;
+    }
+}
+
---
+++ Funciones asincronas/Funciones asincronas/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Threading;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Funciones_asincronas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void EjecutarButton_Click(object sender, EventArgs e)
+        {
+            ResultadoLabel.Text = "Proceso Inciado";
+            HornearPizzaAsyn();
+            ResultadoLabel.Text = "Proceso Terminado";
+        }
+
+        private void HornearPizza()
+        {
+            Thread.Sleep(10000);
+        }
+
+        private Task HornearPizzaAsyn() // Task lo vuelve (asincronico) significa que va a hacer los procesos al mismo tiempo
+        {
+            return Task.Delay(10000);
+        }
+
+        private async void SumarButton_Click(object sender, EventArgs e)
+        {
+            label1.Text = Convert.ToString(await SumarDosValoresAsync(25M, 50M)); 
+        }
+
+        private async Task<decimal> SumarDosValoresAsync(decimal num1, decimal num2)
+        {
+            var resultado = await Task.Run(() =>
+            {
+                return num1 + num2;
+            });
+            return resultado;
+        }
+    }
+}
