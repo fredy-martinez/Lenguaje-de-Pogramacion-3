c3ba2ecf34690bd4764279317532fd6553ab2116
fredy-martinez <90357773+fredy-martinez@users.noreply.github.com>
2021-09-22 14:45:58 -0600

    Arreglos
    Arreglos de numeros

---
+++ Arreglos/Arreglos/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace Arreglos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EjecutrarButton = new System.Windows.Forms.Button();
+            this.ListBox = new System.Windows.Forms.CheckedListBox();
+            this.OrdenarButton = new System.Windows.Forms.Button();
+            this.OrdenarListBox = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // EjecutrarButton
+            // 
+            this.EjecutrarButton.Location = new System.Drawing.Point(56, 44);
+            this.EjecutrarButton.Name = "EjecutrarButton";
+            this.EjecutrarButton.Size = new System.Drawing.Size(132, 55);
+            this.EjecutrarButton.TabIndex = 0;
+            this.EjecutrarButton.Text = "Ejecutar";
+            this.EjecutrarButton.UseVisualStyleBackColor = true;
+            this.EjecutrarButton.Click += new System.EventHandler(this.EjecutrarButton_Click);
+            // 
+            // ListBox
+            // 
+            this.ListBox.FormattingEnabled = true;
+            this.ListBox.Location = new System.Drawing.Point(56, 152);
+            this.ListBox.Name = "ListBox";
+            this.ListBox.Size = new System.Drawing.Size(132, 188);
+            this.ListBox.TabIndex = 1;
+            // 
+            // OrdenarButton
+            // 
+            this.OrdenarButton.Location = new System.Drawing.Point(299, 44);
+            this.OrdenarButton.Name = "OrdenarButton";
+            this.OrdenarButton.Size = new System.Drawing.Size(132, 55);
+            this.OrdenarButton.TabIndex = 2;
+            this.OrdenarButton.Text = "Ordenar";
+            this.OrdenarButton.UseVisualStyleBackColor = true;
+            this.OrdenarButton.Click += new System.EventHandler(this.OrdenarButton_Click);
+            // 
+            // OrdenarListBox
+            // 
+            this.OrdenarListBox.FormattingEnabled = true;
+            this.OrdenarListBox.Location = new System.Drawing.Point(299, 152);
+            this.OrdenarListBox.Name = "OrdenarListBox";
+            this.OrdenarListBox.Size = new System.Drawing.Size(132, 188);
+            this.OrdenarListBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1013, 556);
+            this.Controls.Add(this.OrdenarListBox);
+            this.Controls.Add(this.OrdenarButton);
+            this.Controls.Add(this.ListBox);
+            this.Controls.Add(this.EjecutrarButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button EjecutrarButton;
+        private System.Windows.Forms.CheckedListBox ListBox;
+        private System.Windows.Forms.Button OrdenarButton;
+        private System.Windows.Forms.CheckedListBox OrdenarListBox;
+    }
+}
+
---
+++ Arreglos/Arreglos/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arreglos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void EjecutrarButton_Click(object sender, EventArgs e)
+        {
+            int[] arreglo1 = new int[5];
+
+            arreglo1[0] = 40;
+            arreglo1[1] = 10;
+            arreglo1[2] = 31;
+            arreglo1[3] = 56;
+            arreglo1[4] = 89;
+
+            //otra manera de hacer un arreglo
+            int[] arreglo2 = new int[6] { 1, 2, 3, 4, 5, 7 };
+
+            int[] arreglo3 = new int[] { 1, 3, 4, 5, 6, 7};
+
+            int[] arreglo4 = { 1, 2, 3, 4, 5, 7 };
+
+            string[] arreglo5 = new string[] { "carlos", "Juan" };
+
+            /*for (int i = 0; i<arreglo5.Length; i++)
+            {
+                ListBox.Items.Add(arreglo5[i]);
+            }*/
+            
+            foreach(var item in arreglo5) // Hace lo mismo que el for de arriba
+            {
+                ListBox.Items.Add(item);
+            }
+        }
+
+        private void OrdenarButton_Click(object sender, EventArgs e)
+        {
+            int[] valores = new int[] { 3, 6, 9, 2, 1, 8, 7 };
+
+            //Ordenar de menor a mayor
+           /* Array.Sort(valores);
+
+            foreach(var item in valores)
+            {
+                OrdenarListBox.Items.Add(item);
+            }
+
+            //Los escribe al reves
+            Array.Reverse(valores);
+
+            foreach (var item in valores)
+            {
+                OrdenarListBox.Items.Add(item);
+            }*/
+
+            // para hacerlo de mayor a menor es arreglarlos de menor a mayor y solo imprimirlos al reves así:
+
+            Array.Sort(valores);
+            Array.Reverse(valores);
+
+            foreach (var item in valores)
+            {
+                OrdenarListBox.Items.Add(item);
+            }
+
+        }
+    }
+}
